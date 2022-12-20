b1ad72a5dc6c4c67be022dc11deb72935a591453
cookie025 <1332717484@qq.com>
2022-12-20 21:27:54 +0800

    本地提交测试

--- test.java/src/Main.java
+++ test.java/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("local");
         System.out.println("remote test");
+        System.out.println("local test");
     }
 }
